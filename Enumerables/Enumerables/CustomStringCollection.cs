1a79aff7b10e24328fefedfef3c640eb408c1788
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-02-17 19:12:51 +0200

    Initial implementation - a custom string collection that can be iterated

---
+++ Enumerables/Enumerables/CustomStringCollection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Enumerables
+{
+    public class CustomStringCollection : IEnumerable<string>
+    {
+        private readonly string[] elements;
+
+        public CustomStringCollection(params string[] elements)
+        {
+            this.elements = elements ?? new string[0];
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return ((IEnumerable<string>)elements).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return elements.GetEnumerator();
+        }
+    }
+}
---
+++ Enumerables/Enumerables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Enumerables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customStringCollection = new CustomStringCollection("test1", "test2");
+            foreach (var element in customStringCollection)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
